2e3957ea7b10154d61ed8f18d5721809e3bc5a0c
Franco González <franco.gonzalez.burgos@gmail.com>
2020-06-03 22:35:02 -0300

    Search support for questions added - Can search question by title

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show create]
   resources :questions, only: %i[index show create]
   post '/questions/vote', to: 'questions#vote_question'
+  get '/search/questions', to: 'questions#search_question'
 
   post '/auth/login', to: 'authentication#login'
   post '/auth/renew', to: 'authentication#renew'
