268c7225c0c2118a3d67a0b8a33055f5d1eeedbc
Franco González <franco.gonzalez.burgos@gmail.com>
2020-11-02 15:45:59 -0300

    Added http to production wss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://tututor-backend.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://tututor-client.vercel.app' ]
+  config.action_cable.allowed_request_origins = [ 'https://tututor-client.vercel.app', 'http://tututor-client.vercel.app' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
