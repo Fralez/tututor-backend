164fefa1993ebb15071ac1aa5d5f12bfcd5d5aa5
Franco González <franco.gonzalez.burgos@gmail.com>
2020-11-02 17:54:05 -0300

    Revert last commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
-  before_action :add_allow_credentials_headers
+  # before_action :add_allow_credentials_headers
 
   def not_found
     render json: { errors: 'Not found' }, status: :not_found
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
 
   protected
   def add_allow_credentials_headers
-    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || 'https://tututor-client.vercel.app' # the domain you're making the request from
+    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*' # the domain you're making the request from
   end
 end
