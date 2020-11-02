6d87545471f84f65d15fdfe5ffdd8f1f31aee73b
Franco González <franco.gonzalez.burgos@gmail.com>
2020-11-02 15:28:17 -0300

    Fixed wrong CORS

--- config/application.rb
+++ config/application.rb
@@ -33,17 +33,17 @@ module TututorBackend
     config.generators.system_tests = nil
 
     # CORS support
-    if Rails.env != 'production'
-      config.middleware.insert_before 0, Rack::Cors do
+    config.middleware.insert_before 0, Rack::Cors do
+      if Rails.env != 'production'
         allow do
           origins 'http://localhost:8080'
           resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
         end
+      end
 
-        allow do
-          origins 'https://tututor-client.vercel.app'
-          resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-        end
+      allow do
+        origins 'https://tututor-client.vercel.app'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
       end
     end
 
