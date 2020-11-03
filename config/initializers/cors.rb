c472f826e0de4a5eb6872e6f4924b078ca708e40
Franco González <franco.gonzalez.burgos@gmail.com>
2020-11-02 22:33:39 -0300

    One more try fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,16 @@
 # CORS support
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  if Rails.env != 'production'
+if Rails.env == 'production'
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'https://tututor-client.vercel.app'
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
+  end
+else
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'http://localhost:8080'
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
-  end
-  
-  allow do
-    origins 'https://tututor-client.vercel.app'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
