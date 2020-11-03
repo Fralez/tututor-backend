129f2b9681c49a9e09c32809315f02046c987250
Franco González <franco.gonzalez.burgos@gmail.com>
2020-11-03 14:29:23 -0300

    Revert "Added wildcar to cors without credentials"
    This reverts commit 21701887020f21598f30ceb0434db72b650c46f1.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,11 +2,6 @@
 if Rails.env == 'production'
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*'
-      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
-
-    allow do
       origins 'https://tututor-client.vercel.app'
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
